ea574f52927e0e48f8feecd44845011781370403
Ivan991 <505399048@qq.com>
2017-04-15 22:31:56 +0800

    备份

---
+++ HuXiu4.15/backstage/NewsAdd.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  ValidateRequest="false" CodeFile="NewsAdd.aspx.cs" Inherits="NewsFile_NewsAdd" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+<%--     <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+    <script type="text/javascript" charset="utf-8" src="/ueditor/ueditor.config.js"></script>
+    <script type="text/javascript" charset="utf-8" src="/ueditor/ueditor.all.min.js"> </script>
+    <script type="text/javascript" charset="utf-8" src="/ueditor/lang/zh-cn/zh-cn.js"></script>
+      <style type="text/css">
+        div{
+            width:100%;
+        }
+    </style>--%>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        标题：<asp:TextBox ID="title" MaxLength="45" runat="server" Width="340px" ></asp:TextBox>
+        <br />
+        <br />
+        内容：
+        <br />   
+        <asp:TextBox ID="content" runat="server" MaxLength="45" TextMode="MultiLine" Height="205px" Width="410px"></asp:TextBox>
+        <br />
+        <br />
+        详情链接：<asp:TextBox ID="link" MaxLength="95" runat="server" Width="316px" ></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnAdd" runat="server" Text="增加" OnClick="btnAdd_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ HuXiu4.15/backstage/NewsAdd.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class NewsFile_NewsAdd : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["AdminID"] == null)
+                        Response.Write("<script>alert('账户过期请重新登录！');window.parent.location.href='login.aspx';</script>");
+
+    }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        if (title.Text.Trim().Length > 0 && content.Text.Trim().Length > 0 && link.Text.Trim().Length > 0)
+        {
+            string Pattern = @"((http|https)://)?(www.)?[a-z0-9\.]+(\.(com|net|cn|com\.cn|com\.net|net\.cn))(/[^\s\n]*)?";
+            Regex r= new Regex(Pattern);
+            if (r.IsMatch(link.Text.Trim()))
+            {
+                using (var db = new huxiuEntities())
+                {
+                    News person = new News
+                    {
+                        NewsTitle = title.Text.Trim(),
+                        NewsBody = content.Text,
+                        NewsDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), //发表时间
+                        NewsLink = link.Text.Trim(),
+                        IsDel = false
+                    };
+
+                    db.News.Add(person);
+
+                    if (db.SaveChanges() == 1)
+                        Response.Write("<script>alert('添加成功');location='NewsList.aspx'</script>");
+                    else
+                        Response.Write("<script>alert('添加失败请重试')</script>");
+                }
+            }
+            else
+                Response.Write("<script>alert('请输入正确的网址')</script>");
+
+        }
+        else
+            Response.Write("<script>alert('不能为空')</script>");
+    }
+     
+}
